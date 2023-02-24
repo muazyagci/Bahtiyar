1a5f6ad20eb67a35eb7d7daf4d079a42c33751ff
Muaz Yagci <m.muazyagci@gmail.com>
2023-02-24 15:40:31 +0100

    Day1

--- src/test/java/com/Bahtiyar/page/pages.java
+++ src/test/java/com/Bahtiyar/page/pages.java
@@ -1,5 +1,16 @@
 package com.Bahtiyar.page;
 
-public class pages {
+import com.Bahtiyar.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class pages {
+    public pages() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy (id = "sp-cc-accept")
+    public WebElement acptCookiesBtn;
+    @FindBy (xpath = "(//a[@data-csa-c-type='link'])[1]")
+    public WebElement accountBtn;
 }
--- src/test/java/com/Bahtiyar/step_definitions/Amazon.java
+++ src/test/java/com/Bahtiyar/step_definitions/Amazon.java
@@ -1,6 +1,6 @@
 package com.Bahtiyar.step_definitions;
 
-
+import com.Bahtiyar.page.pages;
 import com.Bahtiyar.utilities.ConfigurationReader;
 import com.Bahtiyar.utilities.Driver;
 import io.cucumber.java.en.Given;
@@ -8,13 +8,15 @@ import io.cucumber.java.en.Then;
 
 
 public class Amazon {
+    pages pages=new pages();
     @Given("user is on the Dashboard")
     public void user_is_on_the_dashboard() {
         Driver.getDriver().get(ConfigurationReader.getProperty("amzn.url"));
+        pages.acptCookiesBtn.click();
     }
     @Given("user clicks to account button")
     public void user_clicks_to_account_button() {
-        
+        pages.accountBtn.click();
     }
     @Given("user clicks to identify button")
     public void user_clicks_to_identify_button() {
