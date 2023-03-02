f6b4df0f7a2aed5a7cae6aa141945e6fbf8fbca0
Muaz Yagci <m.muazyagci@gmail.com>
2023-03-02 21:11:05 +0100

    Day2

---
+++ src/test/java/com/Bahtiyar/page/cydeo.java
@@ -0,0 +1,9 @@
+package com.Bahtiyar.page;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class cydeo extends pages {
+    @FindBy (linkText = "Radio Buttons")
+    public WebElement RadioBtn;
+}
--- src/test/java/com/Bahtiyar/step_definitions/Cydeo_Practice.java
+++ src/test/java/com/Bahtiyar/step_definitions/Cydeo_Practice.java
@@ -3,10 +3,13 @@ package com.Bahtiyar.step_definitions;
 import com.Bahtiyar.utilities.ConfigurationReader;
 import com.Bahtiyar.utilities.Driver;
 import io.cucumber.java.en.When;
+import com.Bahtiyar.page.cydeo;
 
 public class Cydeo_Practice {
+    cydeo cydeo=new cydeo();
     @When("user goes to cydeo practice page")
     public void user_goes_to_cydeo_practice_page() {
         Driver.getDriver().get(ConfigurationReader.getProperty("cydeo.practice"));
+        cydeo.RadioBtn.click();
     }
 }
